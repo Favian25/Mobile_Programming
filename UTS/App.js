8efde703226bf98fe23079e197911444e1bad92e
Ultima <112053892+Favian25@users.noreply.github.com>
2025-05-07 22:45:43 +0700

    Update App.js

--- UTS/App.js
+++ UTS/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import HomeScreen from './components/HomeScreen';
 import RoomScreen from './components/RoomScreen';
 import BookingScreen from './components/BookingScreen';
@@ -13,9 +14,33 @@ export default function App() {
     <PaperProvider>
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Rooms" component={RoomScreen} />
-          <Drawer.Screen name="Booking" component={BookingScreen} />
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Rooms"
+          component={RoomScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="bed-empty" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Booking"
+          component={BookingScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="calendar-edit" color={color} size={size} />
+            ),
+          }}
+        />
         </Drawer.Navigator>
       </NavigationContainer>
     </PaperProvider>
